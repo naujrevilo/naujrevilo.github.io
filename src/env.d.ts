d95dd4e9357a6b7b4a264cf91bb0ec918e643c44
naujrevilo <14898430+naujrevilo@users.noreply.github.com>
2024-01-03 15:38:07 -0500

    Install TinaCMS

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,3 +4,6 @@ declare module 'hero-patterns'
 declare module 'lunr'
 declare module 'photoswipe-dynamic-caption-plugin'
 declare module '@akebifiky/remark-simple-plantuml'
+
+NEXT_PUBLIC_TINA_CLIENT_ID="fce7017a-63ee-487b-b661-f61e03cf011d"
+TINA_TOKEN="d112ff928840d3b6ae825a0614e4df0317c8bf31"
