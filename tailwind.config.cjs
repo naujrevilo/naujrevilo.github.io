77dc4226dd723d698d5d004d9dc2d4dab1508c95
naujrevilo <14898430+naujrevilo@users.noreply.github.com>
2024-01-20 16:07:05 -0500

    :lock: Update Astro and packages - Change logo dark mode
    [Update] Astro 4.1.2 to 4.2 and others
    [Update] Vite to 5.0.12

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,22 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
+    screens: {
+      'sm': '320px',
+      // => @media (min-width: 320px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1200px',
+      // => @media (min-width: 1200px) { ... }
+
+      'xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
+      '2xl': '1600px',
+      // => @media (min-width: 1600px) { ... }
+    },
     extend: {},
   },
   plugins: [
